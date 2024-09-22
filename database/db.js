e56b4b67f674704a803f9ee7b06634320fc86c9a
LeonardoGarcia2005 <120688507+LeonardoGarcia2005@users.noreply.github.com>
2024-09-22 15:09:57 -0500

    Cambios a la hora de generar los pdf

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import Sequelize from "sequelize";
 export const sequelize = new Sequelize(
   "teneria", // db name,
   "postgres", // username
-  "admin", // password
+  "leo2005", // password
   {
     host: "localhost",
     dialect: "postgres",
